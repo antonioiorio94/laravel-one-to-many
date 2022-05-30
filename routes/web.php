2013fa8fefa049945894f1c340134a5e5ac7e17f
Antonio Iorio <antonioiorio94@gmail.com>
2022-05-30 17:02:20 +0200

    add admin routes and edit RouteServiceProvider

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->name('admin.')
+    ->group(function(){
+        Route::get('/', 'HomeController@index')
+        ->name('home');
+});
