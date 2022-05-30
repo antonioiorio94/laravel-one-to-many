08130c563f9cd5a790a893c3acee3fcfcefc9776
Antonio Iorio <antonioiorio94@gmail.com>
2022-05-30 18:24:01 +0200

    add admin and guest view

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,13 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::middleware('auth')
     ->namespace('Admin')
     ->name('admin.')
+    ->prefix('admin')
     ->group(function(){
         Route::get('/', 'HomeController@index')
         ->name('home');
 });
+
+
+Route::get('{any?}', function(){
+    return view('guest.home');
+})->where('any','.*');
