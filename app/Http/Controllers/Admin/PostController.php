6bbdae87bd2b3e4063574423aa5dab680e136730
Antonio Iorio <antonioiorio94@gmail.com>
2022-06-01 19:27:46 +0200

    add destroy

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -119,8 +119,14 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Post $post)
     {
         //
+        if($post){
+            $post->delete();
+        }
+
+
+        return redirect()->route('admin.posts.index');
     }
 }
