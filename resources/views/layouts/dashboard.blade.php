d7a3fbc9bc8ae942ae762ccfc76d9af45cd21fb0
Antonio Iorio <antonioiorio94@gmail.com>
2022-06-01 18:04:20 +0200

    add index

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Boolpress') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -24,7 +24,7 @@
         <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Boolpress</a>
         <ul class="navbar-nav px-3 ml-auto">
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('index') }}">
+                <a class="nav-link" href="{{ route('admin.index') }}">
                     Visita il sito
                 </a>
             </li>
