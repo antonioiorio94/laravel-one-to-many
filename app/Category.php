9ee281052aab1f85a85a6cbdbc8b750ac8648555
Antonio Iorio <antonioiorio94@gmail.com>
2022-06-03 16:07:39 +0200

    edit model

--- app/Category.php
+++ app/Category.php
@@ -8,4 +8,8 @@ class Category extends Model
 {
     //
     protected $table = 'categories';
+
+    public function posts(){
+        return $this->hasMany('App\Post');
+    }
 }
--- app/Post.php
+++ app/Post.php
@@ -8,7 +8,11 @@ use Illuminate\Support\Str;
 class Post extends Model
 {
     //
-    protected $fillable = ['title', 'content', 'slug'];
+    protected $fillable = ['title', 'content', 'slug','category_id'];
+
+    public function category(){
+        return $this->belongsTo('App\Category');
+    }
 
     public static function convertToSlug($title){
         $slugPrefix = Str::slug($title);
